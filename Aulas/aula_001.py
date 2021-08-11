a1e93007a406d14100ac4d9daa036fee05a200b6
Rafaela Henemann <rafaitaly@gmail.com>
2021-08-10 22:41:53 -0300

    Update aula_001.py
    Correções ortográficas e acréscimo de linha

--- Aulas/aula_001.py
+++ Aulas/aula_001.py
@@ -1,4 +1,5 @@
-print('teste de coniguração do VSC com Pyhton')
+print('Teste de configuração do VSC com Pyhton')
 print('Olá, Mundo!')
 n = str(input('Digite seu nome: '))
 print('Olá, ', n)
+# Revisão do Mundo 1 necessária
